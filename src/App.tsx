374c59683db9a20f1b7d518debcfc9dcde7e025a
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-20 20:51:27 +0530

    todoslist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './App.css'
-
 function App() {
+  const [todos,setTodos] = React.useState(["manasa","kavya","priya","smiley"])
+  function abc(){
+    const x = (document.getElementById('dd') as HTMLInputElement).value;
+    setTodos([...todos,x])
+    
+
+    
+  }
+
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+    <input type="text" id='dd' /> &nbsp;
+    <button onClick={abc}>Add List</button>
+    {
+      todos.map((a)=>{
+        return <li>{a}</li>
+      })
+    }
+   
     </>
   )
 }
