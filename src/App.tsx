a7f25cefe72f593f628cbe7dd39a43d3bda1520c
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-20 15:37:51 +0530

    table

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,47 @@
 import './App.css'
 
 function App() {
-
-  
-
-  return (
+  const student = [
+    {
+      firstname : "manasa",
+      lastname : "bathina",
+      gender : "female"
+    },
+    {
+      firstname : "sruthi",
+      lastname : "gundlapalli",
+      gender : "female"
+    },
+    {
+      firstname : "praveen",
+      lastname : "gadde",
+      gender : "male"
+    },
+    {
+      firstname : "maneesha",
+      lastname : "samanthapudi",
+      gender : "female"
+    }
+  ]
+return (
     <>
-      <h1>Welcome to React</h1>
+      <table border={1}>
+        <thead>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Gender</th>
+        </thead>
+        <tbody>
+        {
+          
+            student.map((b)=>{
+              return <tr style={(b.gender==="female")?{backgroundColor:"pink"}:{backgroundColor:"lightblue"}}><td>{b.firstname}</td><td>{b.lastname}</td><td>{b.gender}</td></tr>
+            })
+          
+        }
+        </tbody>
+
+      </table>
     </>
   )
 }
