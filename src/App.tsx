37a8a649929deb268f2ecc04275312d4f069b301
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-20 15:57:40 +0530

    count

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
 import './App.css'
-
 function App() {
+  const [count,setcount] = React.useState(1)
+  
+  function inc(){
+    setcount(count+1)
+  }
+  function dec(){
+    setcount(count-1)
+  }
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+    <h1>{count}</h1>
+    <button onClick={inc}>increment</button>
+    <button onClick={dec}>Decrement</button>
+
     </>
   )
 }
