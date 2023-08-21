60e860bda5ea90e60d8feab26e655a71edb391bf
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 16:28:56 +0530

    eventhandler

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,40 @@
 import './App.css'
 
 function App() {
+  function abc(){
+    document.getElementById('dd').innerHTML = document.getElementById('df').value.toUpperCase()
+  }
 
-  
+  function def(){
+    document.getElementById('d1').innerHTML = document.getElementById('d2').value
+  }
+
+  function ghi(){
+    document.getElementById('d4').innerHTML = document.getElementById('d3').value.toUpperCase()
+  }
+
+  function lmn(){
+    document.getElementById('d5').innerHTML = document.getElementById('d6').value.toUpperCase()
+  }
+
+
 
   return (
     <>
-      <h1>Welcome to React</h1>
+    <input type="text" name="" id="df" />
+    <button onClick={abc}>Click here to see in uppercase</button>
+      <h1 id='dd'></h1>
+
+      <input type="text" onKeyUp={def} id='d2'/>
+      <h1 id='d1'></h1>
+
+      <input type="text" onBlur={ghi} id='d3'/>
+      <h1 id='d4'></h1>
+
+
+      <input type="text" onKeyUp={lmn} id='d6'/>
+      <h1 id='d5'></h1>
+
     </>
   )
 }
