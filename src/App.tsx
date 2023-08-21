6c81d0025989679b4e22aa1c01d91b4b0c507e4c
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 11:31:30 +0530

    flexboxobj

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@
 import './App.css'
 
 function App() {
+  const obj = {
+      name : "manasa",
+      gender : "male",
+      age : 40
+      
+    }
+  
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+    <div style={{border:"1px solid",width:"500px"}}>
+    {
+      
+      Object.entries(obj).map((a)=>{
+        return <div style={(a[0]==="name")?{fontWeight:"bold"}:{fontStyle:"italic"}}>{a[0]}:{a[1]}</div>
+        
+      })
+    }
+    </div>
+
     </>
   )
 }
