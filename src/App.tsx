64e2e4e1b0d7822b63fc54a5e49a5d18f41f8534
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 10:49:18 +0530

    stylingex2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@
 import './App.css'
 
 function App() {
+  const ar = [12,2,45,29]
+  const br = ["manasa","kalyani","smiley"]
+
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+  
+      <h1 style={{color:"red",backgroundColor:"green"}}>Hello Edupoly</h1>
+      <span style={{display:"flex"}}></span>
+      {
+        ar.map((a)=>{
+          return <span style={{border:"1px solid",margin:"10px"}}>{a}</span>
+        })
+      }
+      {
+        br.map((a)=>{
+          return <span style={{border:"1px solid",margin:"10px"}}>{a}</span>
+        })
+
+      }
+    
     </>
   )
 }
