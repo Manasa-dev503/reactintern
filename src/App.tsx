8b7a9be3a70fb4f3d73ab92270fe1c2aef181da4
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 17:52:54 +0530

    eventhandling4

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,39 @@
 import './App.css'
 
 function App() {
+  function abc(){
+    const x = document.getElementById('d1').value
+    const y = document.getElementById('d2').value
+    document.getElementById('dd').innerHTML=x - y
+  }
+  function def(){
+    const a = document.getElementById('d3').value
+    const b = document.getElementById('d4').value
+    document.getElementById('df').innerHTML=a * b
+  }
+  function pqr(){
+    const a = document.getElementById('d3').value
+    const b = document.getElementById('d4').value
+    document.getElementById('df').innerHTML=a / b
+
+  }
+
+  
+  
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+      <input type="text" id='d1' />
+      <input type="text" id='d2' />
+      <button onClick={abc}>Add</button>
+      <h1 id='dd'></h1>
+      <input type="text" id='d3' />
+      <input type="text" id='d4' />
+      <button onClick={def}>Multiply</button>
+      <button onClick={pqr}>Div</button>
+      <h1 id='df'></h1>
     </>
   )
 }
