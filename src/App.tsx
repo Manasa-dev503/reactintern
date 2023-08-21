acb0b5e612c0f38e8d0b7c1c0390556c63899abd
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 13:15:08 +0530

    jsxstylingex3

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import './App.css'
 
 function App() {
 
+  const a = 10;
+  const b = "manasa"
+  const ar = [12,3,14,5,6,9]
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+      <h1 style={{color:"maroon"}}>Hello Edupoly</h1>
+      <h1 style={{color:"pink",backgroundColor:"green"}}>Hello ReactJS</h1>
+      <h1 style={{border:"5px solid black",borderWidth:"20px 10px 20px 10px"}}>Hello Edupoly</h1>
+     <h1 style={(a%2===0)?{color:"green"}:{color:"red"}}>{a}</h1> 
+     <h1 style={(b.length%2===0)?{color:"green"}:{color:"red"}}>{b}</h1> 
+     {
+      ar.map((v)=>{
+        return (
+          <li style={(v%2===0)?{color:"green"}:{color:"red"}}>{v}</li>
+        )
+
+      })
+     }
     </>
   )
 }
