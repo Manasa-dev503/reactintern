7c6c7581fbd3022c393bc96ac5fe09d786142238
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 15:18:20 +0530

    eventhandler

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@
 import './App.css'
 
 function App() {
+  function abc(){
+    alert("Hello Edupoly")
+  }
+  function pqr(){
+    console.log("hello edupoly")
+  }
+  function def(e){
+    console.log(e)
+  }
+  function ghi(e){
+    console.log(e)
+
+  }
+  function lmn(e){
+    console.log(e.target.innerHTML)
+  }
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+      <button onClick={abc}>Click here to see alert</button>
+      <button onClick={pqr}>Click here to see alert</button>
+      <button onClick={def}>Click here to see alert</button>
+      <div style={{width:"300px",height:"200px",border:"2px solid"}} onClick={ghi}>Click here to see alert</div>
+      <div onClick={lmn} style={{width:"300px",height:"200px",border:"2px solid"}}>Welcome to Edupoly ReactJS</div>
+
     </>
   )
 }
