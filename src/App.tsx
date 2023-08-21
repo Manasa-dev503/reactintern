e027746f19a560396f1bdbb5ff66c113f00ea4f6
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 12:05:16 +0530

    arrayobjbox

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,61 @@
 import './App.css'
 
 function App() {
+  const ob = [
+    {
+      firstname : "Praveen",
+      lastname : "Gubbala",
+      Age : 40,
+      Gender : "male"
+    },
+    {
+      firstname : "Smrithi",
+      lastname : "mandana",
+      Age : 25,
+      Gender : "female"
+
+    },
+    {
+      firstname : "mithali",
+      lastname : "raj",
+      Age : 39,
+      Gender : "female"
+    },
+    {
+      firstname : "rohit",
+      lastname : "sharma",
+      Age : 35,
+      Gender : "male"
+    },
+    {
+      firstname : "sachin",
+      lastname : "tendulkar",
+      Age : 50,
+      Gender : "male"
+    }
+  ]
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+    {
+      ob.map((a)=>{
+        return (
+          
+          <div style={{border:"1px solid",margin:"20px"}}><h1 style={(a.Gender==="male")?{backgroundColor:"lightblue"}:{backgroundColor:"pink"}}>Firstname:{a.firstname}<br></br>
+          Lastname:{a.lastname}<br></br>
+          Age:{a.Age}<br></br>
+          Gender:{a.Gender}<br></br>
+
+          </h1>
+          </div>
+          
+        )
+      })
+    }
+  
+      
     </>
   )
 }
