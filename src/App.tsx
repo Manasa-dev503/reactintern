e1f965802d967d1838a1a90ec77a3638d705112d
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-08-21 12:47:51 +0530

    tablesort

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,68 @@
 import './App.css'
 
 function App() {
+  const ob = [
+    {
+      firstname : "manasa",
+      lastname : "bathina",
+      age : "22"
+    },
+    {
+      firstname : "kalyani",
+      lastname : "gadde",
+      age : "28"
+    },
+    {
+      firstname : "kavya",
+      lastname : "reddy",
+      age : "26"
+    },
+    {
+      firstname : "rahul",
+      lastname : "reddy",
+      age : 40
+    }
+  ]
+  function abc(){
+    ob.sort((b,c)=>{
+      return (b.firstname<c.firstname)?-1:1
+
+    })
+  
+
+    
+  }
 
   
 
   return (
     <>
-      <h1>Welcome to React</h1>
+    
+    
+      <table border={1}>
+        <thead>
+          <th onClick={abc}>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </thead>
+        
+        <tbody>
+          {
+          
+          
+            ob.map((v)=>{
+              return <tr><td>{v.firstname}</td><td>{v.lastname}</td><td>{v.age}</td></tr>
+            })
+            
+          }
+
+          
+        </tbody>
+
+        
+      </table>
+
+
     </>
   )
 }
